4c524605ebe00638884be1e77d45652dc4126485
JosephGoff <joeygoff13@gmail.com>
2024-11-13 19:58:35 -0500

    server bug fixed in auth register, now using mysql2 and railway mysql database

--- api/connect.js
+++ api/connect.js
@@ -1,10 +1,18 @@
-import mysql from "mysql"
+import mysql from "mysql2"
 import dotenv from 'dotenv';
 dotenv.config();
 
+// export const db = mysql.createConnection({
+//     host: "aws-db2.c9wkcmm26naa.us-east-2.rds.amazonaws.com",
+//     user: "admin",
+//     password: process.env.DB_PW,
+//     database: "social"
+// })
+
 export const db = mysql.createConnection({
-    host: "aws-db2.c9wkcmm26naa.us-east-2.rds.amazonaws.com",
-    user: "admin",
-    password: process.env.DB_PW,
-    database: "social"
-})
+    host: 'junction.proxy.rlwy.net',
+    port: 51271,
+    user: 'root',
+    password: 'RnpAIoKFQoSvJzUKlNgyYxpYvNFOnsRG',
+    database: 'social' 
+});
--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 
 app.use(express.json())
 app.use(cors({
-  origin: "http://localhost:3000" || "https://inspireconnect.online",
+  origin: "http://localhost:3000" || "https://inspireconnect.site",
   credentials: true,
   // exposedHeaders: ['Authorization'],
 }))
